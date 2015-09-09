6aa7b269f2f1e8716c69b93111c734ab2797e01f
Reece Bedding <reecebedding@hotmail.co.uk>
2015-09-09 23:29:32 +0100

    Added angular scripts setup application

---
+++ js/app/browserController.js
@@ -0,0 +1,7 @@
+(function(angular){
+	var module = angular.module("apiBrowser", []);
+
+	module.controller("browserController", ["$scope", "$http", "$window", function($scope, $http, $window){
+		
+	}]);
+})(window.angular);
