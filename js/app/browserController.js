2379ed4a1d15345d036bbdd7be263d21e67fd21f
Reece Bedding <reecebedding@hotmail.co.uk>
2015-09-10 00:30:33 +0100

    Added functionaliy to select API version and corresponding endpoints

--- js/app/browserController.js
+++ js/app/browserController.js
@@ -1,7 +1,33 @@
 (function(angular){
-	var module = angular.module("apiBrowser", []);
+	var module = angular.module("apiBrowser", ["ngResource"]);
 
-	module.controller("browserController", ["$scope", "$http", "$window", function($scope, $http, $window){
+	module.controller("browserController", ["$scope", "$http", "$window", "$resource", function($scope, $http, $window, $resource){
+
+		CreateServices($resource);
+
+		$scope.versions = ["v1", "v2"];
+		$scope.selectedVersion = "v2";
 		
+		$scope.endpoints = v2ApiStaticData.endpoints;
+		$scope.selectedEndpoint = {};
+
+
+		$scope.LoadVersionEndpoints = function(){
+			switch($scope.selectedVersion){
+				case "v1":
+					$scope.endpoints = v1ApiStaticData.endpoints;
+					break;
+				case "v2":
+					$scope.endpoints = v2ApiStaticData.endpoints;
+					break;
+				default:	
+					$scope.endpoints = v2ApiStaticData.endpoints;
+					break;
+			}			
+		}
+
+		function CreateServices($resource){
+				
+		}
 	}]);
 })(window.angular);
