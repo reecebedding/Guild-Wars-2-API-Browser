d00b6b397048e1c552c625c115c2563441b9fc27
Reece Bedding <reecebedding@hotmail.co.uk>
2015-09-13 03:13:11 +0100

    Added validation to fields before searching

--- js/app/browserController.js
+++ js/app/browserController.js
@@ -23,20 +23,51 @@
 					break;
 			}			
 		}
-
+		
 		$scope.SearchAPI = function(){						
-			var formattedUrl = "https://api.guildwars2.com" + $scope.selectedEndpoint.url;
-			$scope.apiData = "";			
+			if(ValidateFields()){
+				var formattedUrl = "https://api.guildwars2.com" + $scope.selectedEndpoint.url;
+				$scope.apiData = "";			
 
-			var request = CreateCORSRequest(formattedUrl);
-			request.onload = function(){
-				$scope.apiData = request.responseText;
-				$scope.$apply();
-			};
-			request.onerror = function(){
-			};			
-			request.send();
+				var request = CreateCORSRequest(formattedUrl);
+				request.onload = function(){
+					$scope.apiData = request.responseText;
+					$scope.$apply();
+				};
+				request.onerror = function(){
+				};			
+				request.send();
+			}
 		}
+
+		$scope.anyFieldErrors = false;
+		function ValidateFields(){
+			var valid = true;
+			if($("#apiVersion option:selected").text().trim().length == 0){
+				$("#apiVersion").parent().addClass('has-error');
+				valid = false;
+			}else{
+				$("#apiVersion").parent().removeClass('has-error');
+			}
+			if($("#apiEndpoint option:selected").text().trim().length == 0){
+				$("#apiEndpoint").parent().addClass('has-error');
+				valid = false;
+			}else{
+				$("#apiEndpoint").parent().removeClass('has-error');
+			}
+			$(".parameter input").each(function(index){
+				if($(this).attr('data-fieldrequired') == "true"){
+					if($(this).val().trim().length == 0){
+						$(this).parent().addClass('has-error');
+						valid = false;
+					}else{
+						$(this).parent().removeClass('has-error');
+					}
+				}
+			});
+			$scope.anyFieldErrors = !valid;
+			return valid;
+		}		
 
 		function CreateCORSRequest(url){
 			var request = new XMLHttpRequest();
